b6fbc7e5a182b2707a479fe28cde02365d0e8264
parksohyeon <dso753@elinmedia.com>
2020-12-09 02:00:55 +0900

    04 리액트 네이티브 컴포넌트 > 네이티브 컴포넌트 > Text

---
+++ src/components/SmallText.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    StyleSheet,
+    Text
+} from 'react-native';
+import AppText from './AppText';
+
+const SmallText = ({ children, style, ...rest }) => (
+    <AppText style={[styles.small, style]} {...rest}>
+        {children}
+    </AppText>
+);
+
+SmallText.propTypes = {
+    style: Text.propTypes.style,
+    children: PropTypes.node
+}
+
+const styles = StyleSheet.create({
+    small: {
+        fontSize: 11
+    }
+});
+
+export default SmallText;
